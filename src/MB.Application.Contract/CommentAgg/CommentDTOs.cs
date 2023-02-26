b67f99d65e305ad6dd117b062a860d2dc5a4dfcb
hamidba <balalzadehhamid79@gmail.com>
2023-02-26 21:32:42 +0330

    Add Abstractions Of Comment Use Cases

---
+++ src/MB.Application.Contract/CommentAgg/CommentDTOs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MB.Domain.CommentAgg.Helpers;
+
+namespace MB.Application.Contract.CommentAgg;
+
+public class CommentListDto
+{
+    public ulong Id { get;  set; }
+    public string? FullName { get;  set; }
+    public string? Email { get;  set; }
+    public string? Content { get;  set; }
+    public CommentStatus Status { get;  set; }
+    public string? ArticleTitle { get;  set; } 
+    public string? CreationDate { get;  set; }
+}
+
+public class CreateCommentCommand
+{
+    public ulong ArticleId { get;  set; }
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Content { get; set; }
+}
---
+++ src/MB.Application.Contract/CommentAgg/ICommentApplication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Framework.Application;
+using MB.Domain.CommentAgg.Helpers;
+
+namespace MB.Application.Contract.CommentAgg;
+
+public interface ICommentApplication
+{
+    List<CommentListDto> GetList();
+    OperationResult ChangeStatus(CommentStatus status);
+    OperationResult Create(CreateCommentCommand command);
+}
