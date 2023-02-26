5149c610cefb112cfe9609dac0cc760fc9db5a6d
hamidba <balalzadehhamid79@gmail.com>
2023-02-26 21:18:26 +0330

    Add Abstractions Of Category Use Cases

---
+++ src/MB.Application.Contract/CategoryAgg/CategoryDTOs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MB.Application.Contract.CategoryAgg;
+
+public class CategoryListDto
+{
+    public ulong Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDelete { get; set; }
+    public string? CreationDate { get; set; }    
+}
+
+public class CreateCategoryCommand
+{
+    public string? Title { get; set; } 
+}
+
+public class EditCategoryCommand : CreateCategoryCommand
+{
+    public ulong Id { get; set; }
+}
---
+++ src/MB.Application.Contract/CategoryAgg/ICategoryApplication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Framework.Application;
+
+namespace MB.Application.Contract.CategoryAgg;
+
+public interface ICategoryApplication
+{
+    List<CategoryListDto> GetList();
+    OperationResult ChangeStatus(bool isDelete);
+    OperationResult Create(CreateCategoryCommand command);
+    OperationResult Edit(EditCategoryCommand command);
+}
